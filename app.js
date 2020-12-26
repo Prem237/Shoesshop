e1df6e306708823567cb59c00cd45f331da66b3f
Prem kumar <premkumar.selvam@nutechnologyinc.com>
2020-12-26 12:55:17 +0530

    Updates

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -20,12 +22,46 @@ var _reportWebVitals2 = _interopRequireDefault(_reportWebVitals);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var temp = _react2.default.createElement(
-  'h1',
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var Header = function (_React$Component) {
+  _inherits(Header, _React$Component);
+
+  function Header() {
+    _classCallCheck(this, Header);
+
+    return _possibleConstructorReturn(this, (Header.__proto__ || Object.getPrototypeOf(Header)).apply(this, arguments));
+  }
+
+  _createClass(Header, [{
+    key: 'render',
+    value: function render() {
+      return _react2.default.createElement(
+        'div',
+        null,
+        'See the details'
+      );
+    }
+  }]);
+
+  return Header;
+}(_react2.default.Component);
+
+var code = _react2.default.createElement(
+  'div',
   null,
-  'WELCOME!!!'
+  _react2.default.createElement(
+    'h1',
+    null,
+    'Welcome To All!!!!!!!!!'
+  ),
+  _react2.default.createElement(Header, null)
 );
-_reactDom2.default.render(temp, document.getElementById('root'));
+_reactDom2.default.render(code, document.getElementById('root'));
 _reactDom2.default.render(_react2.default.createElement(
   _react2.default.StrictMode,
   null,
